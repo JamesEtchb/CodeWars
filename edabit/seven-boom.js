50846e5c7be54d207dde2f71163b142579d34716
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-22 12:56:42 -0400

    fixed the problem and cleaned up the code a bit

--- edabit/seven-boom.js
+++ edabit/seven-boom.js
@@ -1,15 +1,36 @@
-let arr = [ 1,2,7]
+let arr = [1, 2, 7]
+
+// function sevenBoom(arr) {
+// 	let str = arr.join()
+// 	let newsplit = str.split('')
+// 	for(let i = 0; i < newsplit.length; i++){
+// 		if ( arr[i] === '7'){
+// 			return 'Boom!'
+// 		} else {
+// 			return 'there is no 7 in the array'
+// 		}
+// 	}
+// }
+
+// //this is another way to write it
+// function sevenBoom(arr) {
+
+//     if (arr.includes(7)){
+//         return 'Boom!'
+//     } else {
+//         return 'there is no 7 in the array'
+//     }
+
+// }
+// sevenBoom(arr)
 
 function sevenBoom(arr) {
-	let str = arr.join()
-	let newsplit = str.split('')
-	for(let i = 0; i < newsplit.length; i++){
-		if ( arr[i] === 7){
-			return 'Boom!'
-		} else {
-			return 'there is no 7 in the array'
-		}
-	}
+  str = arr.join("")
+  if (str.includes(7)) {
+    return "Boom!"
+  } else {
+    return "there is no 7 in the array"
+  }
 }
 
-sevenBoom(arr)
+console.log(sevenBoom([35, 4, 9, 37]))
