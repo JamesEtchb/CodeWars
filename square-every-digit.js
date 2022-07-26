dbccd90997c319a559d234f4e54d59a3f22c3ace
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-26 10:28:02 -0400

    added new function that squares all the numbers individually in any number given

---
+++ square-every-digit.js
@@ -0,0 +1,10 @@
+function squareDigits(num){
+	num = num.toString()
+  let newStr = ""
+  for (let i = 0; i < num.length; i++){
+    newStr += num[i] * num[i]
+  }
+  return Number(newStr)
+}
+
+console.log(squareDigits(9119))
